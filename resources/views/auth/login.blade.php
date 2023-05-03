76b3865013540325a3359fc9012fe65acf8f57d3
frknh <frknhzr344423@gmail.com>
2023-05-03 23:56:23 +0300

    commit 2.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,7 +68,7 @@
                                     </div>
 
                                     <a class="small text-muted" href="#!">Şifremi unuttum</a>
-                                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Hesabın var mı? <a href="#!" style="color: #393f81;">Üye Ol</a></p>
+                                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Hesabın var mı? <a href="{{route('login')}}" style="color: #393f81;">Üye Ol</a></p>
                                     <a href="#!" class="small text-muted">Kullanım şartları.</a>
                                     <a href="#!" class="small text-muted">Gizlilik Politikası</a>
                                 </form>
